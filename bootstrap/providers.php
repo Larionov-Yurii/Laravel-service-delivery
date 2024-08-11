fbf785d8b50da5c167783ec420fafa352165875e
Larionov-Yurii <larionov.yurii.work@gmail.com>
2024-08-12 01:51:03 +0300

    Updated files

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,5 +1,9 @@
 <?php
+/**
+ * Registers the application's service providers, including the CourierServiceProvider.
+ */
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\CourierServiceProvider::class,
 ];
--- config/services.php
+++ config/services.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Stores configuration details for third-party services and couriers,
+ * including URLs, credentials, and class mappings.
+ */
 
 return [
 
@@ -35,4 +39,34 @@ return [
         ],
     ],
 
+    'twilio' => [
+        'sid' => env('TWILIO_SID'),
+        'token' => env('TWILIO_AUTH_TOKEN'),
+        'from' => env('TWILIO_PHONE_NUMBER'),
+    ],
+
+    'admin_email' => env('ADMIN_EMAIL', 'admin@example.com'),
+
+    'justin' => [
+        'url' => env('JUSTIN_URL', 'http://justin.test/api/delivery/justin'),
+        'sender_address' => env('JUSTIN_SENDER_ADDRESS', 'Kiev, St. Butlerova, 1'),
+    ],
+
+    'ukrposhta' => [
+        'url' => env('UKRPOSHTA_URL', 'http://ukrposhta.test/api/delivery/ukrposhta'),
+        'sender_address' => env('UKRPOSHTA_SENDER_ADDRESS', 'Kyiv. St. Soborna, 98'),
+    ],
+
+    'novaposhta' => [
+        'url' => env('NOVAPOSHTA_URL', 'http://novaposhta.test/api/delivery/novaposhta'),
+        'sender_address' => env('NOVAPOSHTA_SENDER_ADDRESS', 'Kyiv, St. Pirogovsky Shlyach, 135'),
+    ],
+
+    'courier_classes' => [
+        'justin' => \App\Services\JustinCourier::class,
+        'ukrposhta' => \App\Services\UkrPoshtaCourier::class,
+        'novaposhta' => \App\Services\NovaPoshtaCourier::class,
+    ],
+
+    'default_courier' => env('DEFAULT_COURIER', 'novaposhta'),
 ];
